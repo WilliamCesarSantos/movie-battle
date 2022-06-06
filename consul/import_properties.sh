bf306f0d4ebf7748ca0d0d73cf0766a91d87ba75
William Cesar Santos <william_cesar_santos@hotmail.com>
2022-06-05 23:14:58 -0300

    Developing

--- consul/import_properties.sh
+++ consul/import_properties.sh
@@ -1,6 +1,3 @@
 # /bin/bash
-cat application.properties | jq -R -s 'split("\n") | map(select(length > 0)) | map(select(startswith("#") | not)) | map(split("=")) | map({(.[0]): .[1]}) | add' > app_config.json
-<app_config.json jq '. | to_entries | . | map({key:.key, value:(.value| @base64)})' > app_config_base64.json
-consul kv import @app_config_base64.json
 
-# cat app_config_base64.json | consul kv import -prefix=movie-battle
+curl -XPUT --data-binary @application.properties http://localhost:8500/v1/kv/movie-battle/application.properties
